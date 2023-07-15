638de19a47aef9878c37c9b2539c006760f5974f
Franco-611 <69815571+Franco-611@users.noreply.github.com>
2023-07-15 16:17:27 -0600

    Reconocer tokens

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from antlr4 import FileStream, CommonTokenStream
+from antlr4 import *
 from dist.yaplLexer import yaplLexer
 from dist.yaplParser import yaplParser
 
@@ -8,9 +8,23 @@ lexer = yaplLexer(input_stream)
 token_stream = CommonTokenStream(lexer)
 
 token = lexer.nextToken()
-while token.type == yaplLexer.EOF:
-    print(token.text)
+
+list_tokens = []
+
+while token.type != Token.EOF:
+
+    if token.type == token.INVALID_TYPE:
+    ##if token.type == lexer.ERROR:
+        print("Error léxico en línea", token.line, ": ", token.text)
+        break
+
+    else:
+        token_pair = [lexer.symbolicNames[token.type], token.text, token.line]
+        list_tokens.append(token_pair)
+
     token = lexer.nextToken()
+
+print(list_tokens)
 
 parser = yaplParser(token_stream)
 tree = parser.program()
