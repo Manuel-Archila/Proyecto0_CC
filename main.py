8a67815970497b9bf247a13baaf84071c432c257
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2023-07-16 13:08:38 -0600

    Scanner completo

--- main.py
+++ main.py
@@ -11,12 +11,29 @@ token = lexer.nextToken()
 
 list_tokens = []
 
+MAX_STRING_LENGTH = 25
+
 while token.type != Token.EOF:
 
-    if token.type == token.INVALID_TYPE:
-    ##if token.type == lexer.ERROR:
+    #if token.type == token.ERROR:
+    if token.type == lexer.ERROR:
         print("Error léxico en línea", token.line, ": ", token.text)
-        break
+
+        token_pair = [lexer.symbolicNames[lexer.ERROR], token.text, token.line]
+        list_tokens.append(token_pair)
+    
+    elif token.type == lexer.STRING:
+        if len(token.text) > MAX_STRING_LENGTH:
+            print("Error: Tamaño de lexema excedido en linea", token.line, ": ", token.text)
+            token_pair = [lexer.symbolicNames[lexer.ERROR], token.text, token.line]
+            list_tokens.append(token_pair)
+        elif '\n' in token.text:
+            print("Error: Nueva linea detectado dentro del String en linea ", token.line, ": ", token.text)
+            token_pair = [lexer.symbolicNames[lexer.ERROR], token.text, token.line]
+            list_tokens.append(token_pair)
+        else:
+            token_pair = [lexer.symbolicNames[token.type], token.text, token.line]
+            list_tokens.append(token_pair)        
 
     else:
         token_pair = [lexer.symbolicNames[token.type], token.text, token.line]
@@ -24,7 +41,7 @@ while token.type != Token.EOF:
 
     token = lexer.nextToken()
 
-print(list_tokens)
+#print(list_tokens)
 
 parser = yaplParser(token_stream)
 tree = parser.program()
