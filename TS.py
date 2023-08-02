35692afe939c922dad8f667e8a9cb361e527c5a5
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2023-08-02 11:56:19 -0600

    Arreglos a tabla de simbolos

--- TS.py
+++ TS.py
@@ -1,17 +1,16 @@
 class TS:
     def __init__(self):
-        self.table = {}
+        self.table = []
 
     def agregar_simbolo(self, lexema, linea, columna, token):
-        counter = sum(1 for x in self.table if x == lexema)
-        par = (lexema, )
-        if lexema in self.table:
-            self.table[lexema].append({'linea': linea, 'columna': columna, 'token': token})
-        else:
-            self.table[lexema] = [{'linea': linea, 'columna': columna, 'token': token}]
+        self.table.append({'lexema': lexema, 'linea': linea, 'columna': columna, 'token': token})
 
     def buscar_simbolo(self, lexema):
-        return self.table.get(lexema, None)
+        lexemas = []
+        for i in self.table:
+            if i['lexema'] == lexema:
+                lexemas.append(i)
+        return lexemas
 
     def get_table(self):
         return self.table
