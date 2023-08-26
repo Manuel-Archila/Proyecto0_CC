4f25d71f0b9050a8388fda2c3ed37dd9282ac425
Franco-611 <69815571+Franco-611@users.noreply.github.com>
2023-08-25 21:31:45 -0600

    SABER
    Co-authored-by: Manuel-Archila <Manuel-Archila@users.noreply.github.com>

--- SemanticAnalyzer.py
+++ SemanticAnalyzer.py
@@ -1,84 +1,210 @@
 from dist.yaplVisitor import yaplVisitor
-from SymbolTable import SymbolTable, Symbol
 from dist.yaplParser import yaplParser
+from antlr4.tree.Tree import TerminalNode
+from SymbolTable import Symbol, SymbolTable
 
 class SemanticAnalyzerMio(yaplVisitor):
     def __init__(self, symbol_table):
         self.symbol_table = symbol_table
 
     def visit_program(self, ctx:yaplParser.ProgramContext):
-        self.visitChildren(ctx)
-        
-        return
-
-    # Para declaraciones de clases
+        return self.visitChildren(ctx)
+    
     def visitClass(self, ctx:yaplParser.ClassContext):
-        class_name = ctx.TYPE()[0].getText()
-        symbol = Symbol(name=class_name, line=ctx.start.line, symbol_type="class", scope=self.symbol_table.current_scope())
-        self.symbol_table.add_symbol(symbol)
-        
-        # Entrar a un nuevo ámbito para el cuerpo de la clase
-        self.symbol_table.enter_scope(class_name)
-        
-        # Visitar las características de la clase (métodos/variables)
-        self.visitChildren(ctx)
-        
-        # Salir del ámbito de la clase después de visitar su cuerpo
-        self.symbol_table.exit_scope()
-        return
-
-    # Para declaraciones de variables
+        self.symbol_table.put(ctx.TYPE()[0].getText(), ctx.start.line, "class")
+        self.symbol_table.push_scope()
+        temp = self.visitChildren(ctx)
+        self.symbol_table.pop_scope()
+        return temp
+    
     def visitFeature(self, ctx:yaplParser.FeatureContext):
-        if ctx.ID() and ctx.TYPE():
-            var_name = ctx.ID().getText()
-            var_type = ctx.TYPE().getText()
-            symbol = Symbol(name=var_name, line=ctx.start.line, symbol_type="variable", data_type=var_type, scope=self.symbol_table.current_scope())
-            self.symbol_table.add_symbol(symbol)
 
-    def visitExpr(self, ctx:yaplParser.ExprContext):
-        if ctx.ASSIGN():
-            var_name = ctx.ID().getText()
-            symbol = self.symbol_table.get_symbol(var_name)
-            if not symbol:
-                raise Exception(f"Variable {var_name} not declared at line {ctx.start.line}")
+        if ctx.LPAR():
+            self.symbol_table.put(ctx.ID().getText(), ctx.start.line, "function", ctx.TYPE().getText())
 
-            # Suponiendo que tienes un método get_expr_type
-            right_expr_type = self.get_expr_type(ctx.expr())
-            if symbol.data_type != right_expr_type:
-                raise Exception(f"Type mismatch for variable {var_name} at line {ctx.start.line}. Expected {symbol.data_type} but got {right_expr_type}")
+            self.symbol_table.push_scope()
+            temp = self.visitChildren(ctx)
+            self.symbol_table.pop_scope()
 
-        elif ctx.DOT():
-            method_name = ctx.ID().getText()
-            symbol = self.symbol_table.get_symbol(method_name)
-            if not symbol or symbol.symbol_type != "method":
-                raise Exception(f"Method {method_name} not declared or used incorrectly at line {ctx.start.line}")
+            return temp
 
-            # Suponiendo que el símbolo para un método tiene un atributo 'arguments'
-            passed_args = ctx.expr()  
-            if len(passed_args) != len(symbol.arguments):
-                raise Exception(f"Incorrect number of arguments for method {method_name} at line {ctx.start.line}")
+        else:
+            self.symbol_table.put(ctx.ID().getText(), self.get_line(ctx), "atribute")
 
-            for idx, arg in enumerate(passed_args):
-                arg_type = self.get_expr_type(arg)
-                if arg_type != symbol.arguments[idx].data_type:
-                    raise Exception(f"Type mismatch for argument {idx+1} of method {method_name} at line {ctx.start.line}. Expected {symbol.arguments[idx].data_type} but got {arg_type}")
-
+            return self.visitChildren(ctx)
+    
     def visitFormal(self, ctx:yaplParser.FormalContext):
-        arg_name = ctx.ID().getText()
-        arg_type = ctx.TYPE().getText()
-        
-        existing_symbol = self.symbol_table.get_symbol(arg_name)
-        if existing_symbol and existing_symbol.scope == self.symbol_table.current_scope():
-            raise Exception(f"Argument {arg_name} already declared at line {ctx.start.line}")
-        
-        symbol = Symbol(name=arg_name, line=ctx.start.line, symbol_type="argument", data_type=arg_type, scope=self.symbol_table.current_scope())
-        self.symbol_table.add_symbol(symbol)
+        self.symbol_table.put(ctx.ID().getText(), ctx.start.line, "variableT", ctx.TYPE().getText())
 
-    # Este es un esqueleto para get_expr_type. Deberías expandir esto según las necesidades de tu lenguaje.
-    def get_expr_type(self, ctx):
-        if ctx.ID():
-            symbol = self.symbol_table.get_symbol(ctx.ID().getText())
-            if symbol:
-                return symbol.data_type
-        # ... agregar más lógica para otros tipos de expresiones
-        return None  # Devuelve None si no se puede determinar el tipo
+        return None
+    
+    def visitExpr(self, ctx:yaplParser.ExprContext):
+
+        print(ctx.getText())
+        
+        if ctx.IF():
+            self.symbol_table.push_scope()
+            temp = self.visitChildren(ctx)
+            self.symbol_table.pop_scope()
+            return temp
+
+        if ctx.ELSE():
+            self.symbol_table.push_scope()
+            temp = self.visitChildren(ctx)
+            self.symbol_table.pop_scope()
+            return temp
+
+        if ctx.WHILE():
+            self.symbol_table.push_scope()
+            temp = self.visitChildren(ctx)
+            self.symbol_table.pop_scope()
+            return temp
+
+        if ctx.LET():
+            for i in range(len(ctx.ID())):
+                self.symbol_table.put(ctx.ID()[i], self.get_line(ctx), "variable",ctx.TYPE()[i].getText())
+                for chil in ctx.getChildren():
+                    print(chil.getText())
+                return self.visitChildren(ctx)
+
+        else:
+            return self.visitChildren(ctx)
+    # # Para declaraciones de clases
+    # def visitClass(self, ctx:yaplParser.ClassContext):
+    #     class_name = ctx.TYPE()[0].getText()
+    #     line = self.get_line(ctx.TYPE()[0])
+    #     self.symbol_table.put(class_name, line, "class")
+    #     self.symbol_table.push_scope()
+    #     ret = self.visitChildren(ctx)
+    #     self.symbol_table.pop_scope()
+
+    #     return ret
+
+    # def visitFeature(self, ctx:yaplParser.FeatureContext):
+    #     if ctx.LPAR():
+    #         self.visitMethod(ctx)
+    #     else:
+    #         self.visitAttribute(ctx)
+
+
+    # def visitMethod(self, ctx:yaplParser.FeatureContext):
+    #         if ctx.ID() and ctx.TYPE():
+    #             var_name = ctx.ID().getText()
+    #             var_type = ctx.TYPE().getText()
+    #             line = self.get_line(ctx.ID())
+    #             self.symbol_table.put(var_name, line, "function", var_type)
+
+
+    #             for formal in ctx.formal():
+    #                 self.symbol_table.push_scope()
+    #                 self.visitFormal(formal)
+    #                 self.symbol_table.pop_scope()
+
+    #             if ctx.expr():
+    #                 self.symbol_table.push_scope()
+    #                 self.visitExpr(ctx.expr())
+    #                 self.symbol_table.pop_scope()
+
+    #             return self.visitChildren(ctx)
+    #         else:
+    #             print("Error en la declaracion de metodos")
+
+    # def visitAttribute(self, ctx:yaplParser.FeatureContext):
+    #     self.symbol_table.put(ctx.ID().getText(), self.get_line(ctx.ID()), "attribute", ctx.TYPE().getText())
+
+
+    # def visit_Expr(self, ctx:yaplParser.ExprContext):
+
+    #     if ctx.IF():
+    #         node_type = ctx.getText()
+    #         line = self.get_line(ctx)
+
+    #         for hijo in ctx.getChildren():
+    #             if not isinstance(hijo, TerminalNode):
+    #                 self.visit_Expr(hijo)
+    #             else:
+    #                 self.visitExpr(hijo)
+
+    #         # print("Anadi un una expresion a la tabla")
+    #         self.symbol_table.put(node_type, line, ctx, "if")
+
+    #     for hijo in ctx.getChildren():
+    #         if not isinstance(hijo, TerminalNode):
+    #             self.visitExpr(hijo)
+    #         else:
+    #             self.verify_expr(hijo)
+    #     # if ctx.ASSIGN():
+    #     #     var_name = ctx.ID()[0].getText()
+    #     #     self.symbol_table.put(var_name, self.get_line(ctx.ID()[0]), "variable", self.get_expr_type(ctx.expr()))
+    #     #     # if var_name in self.symbol_table.get_table():
+    #     #     #     raise Exception(f"Variable {var_name} not declared at line {ctx.start.line}")
+
+    #     #     # # Suponiendo que tienes un método get_expr_type
+    #     #     # right_expr_type = self.get_expr_type(ctx.expr())
+    #     #     # if symbol.data_type != right_expr_type:
+    #     #     #     raise Exception(f"Type mismatch for variable {var_name} at line {ctx.start.line}. Expected {symbol.data_type} but got {right_expr_type}")
+
+    #     # elif ctx.DOT():
+    #     #     method_name = ctx.ID()[0].getText()
+    #     #     symbol = self.symbol_table.get_symbol(method_name)
+    #     #     if not symbol or symbol.symbol_type != "method":
+    #     #         raise Exception(f"Method {method_name} not declared or used incorrectly at line {ctx.start.line}")
+
+    #     #     # Suponiendo que el símbolo para un método tiene un atributo 'arguments'
+    #     #     passed_args = ctx.expr()  
+    #     #     if len(passed_args) != len(symbol.arguments):
+    #     #         raise Exception(f"Incorrect number of arguments for method {method_name} at line {ctx.start.line}")
+
+    #     #     for idx, arg in enumerate(passed_args):
+    #     #         arg_type = self.get_expr_type(arg)
+    #     #         if arg_type != symbol.arguments[idx].data_type:
+    #     #             raise Exception(f"Type mismatch for argument {idx+1} of method {method_name} at line {ctx.start.line}. Expected {symbol.arguments[idx].data_type} but got {arg_type}")
+
+    #     # elif ctx.ID():
+    #     #     var_name = ctx.ID().getText()
+    #     #     symbol = self.symbol_table.get_symbol(var_name)
+    #     #     if not symbol:
+    #     #         raise Exception(f"Variable {var_name} not declared at line {ctx.start.line}")
+
+    # def visitExpr(self, ctx:yaplParser.ExprContext):
+
+        
+
+    #     node_type = ctx.getText()
+    #     line = self.get_line(ctx)
+
+    #     # print("Anadi un una expresion a la tabla")
+    #     self.symbol_table.put(node_type, line, ctx)
+
+
+
+    # def visitFormal(self, ctx:yaplParser.FormalContext):
+    #     arg_name = ctx.ID().getText()
+    #     arg_type = ctx.TYPE().getText()
+        
+    #     existing_symbol = self.symbol_table.get_symbol(arg_name)
+    #     if existing_symbol and existing_symbol.scope == self.symbol_table.current_scope():
+    #         raise Exception(f"Argument {arg_name} already declared at line {ctx.start.line}")
+        
+    #     symbol = Symbol(name=arg_name, line=ctx.start.line, symbol_type="argument", data_type=arg_type, scope=self.symbol_table.current_scope())
+    #     self.symbol_table.add_symbol(symbol)
+
+    # # Este es un esqueleto para get_expr_type. Deberías expandir esto según las necesidades de tu lenguaje.
+    # def get_expr_type(self, ctx):
+    #     if ctx[0].ID():
+    #         symbol = self.symbol_table.get_symbol(ctx.ID().getText())
+    #         if symbol:
+    #             return symbol.data_type
+    #     # ... agregar más lógica para otros tipos de expresiones
+    #     return None  # Devuelve None si no se puede determinar el tipo
+
+    def get_line(self, node):
+        # Si el nodo es un TerminalNode, se puede obtener la línea directamente.
+        if isinstance(node, TerminalNode):
+            return node.symbol.line
+        # De lo contrario, si es un nodo de una regla, se utiliza `start`.
+        elif hasattr(node, 'start'):
+            return node.start.line
+        # Si el nodo no tiene información de línea, devuelve None o cualquier valor por defecto.
+        else:
+            return None
--- SymbolTable.py
+++ SymbolTable.py
@@ -10,44 +10,18 @@ class Symbol:
         return str(self.name) + " " + str(self.line) + " " + str(self.symbol_type) + " " + str(self.data_type) + " " + str(self.scope)
 
 
-class Scope:
-    def __init__(self, parent=None):
-        self.symbols = {}  # Dictionary to store symbols by name
-        self.parent = parent  # Reference to the parent scope
-
-    def put(self, name, symbol):
-        """Add a symbol to the current scope."""
-        self.symbols[name] = symbol
-
-    def get(self, name):
-        """Get a symbol from this scope or its parent scopes."""
-        symbol = self.symbols.get(name)
-        if symbol:
-            return symbol
-        elif self.parent:
-            return self.parent.get(name)
-        else:
-            return None
-
-    def remove(self, name):
-        """Remove a symbol from the current scope."""
-        del self.symbols[name]
-    
-    def __repr__(self):
-        return str(self.symbols)
-
-
 class SymbolTable:
     def __init__(self):
-        self.stack = [Scope()]  # Initialize with a global scope
-        self.full_stack = [Scope()]
+        self.scopeTOTAL = 0
+        self.scope = 0  # Initialize with a global scope
+        self.stack = [Scope()]  # List of scopes, the last one is the current scope
+        self.actual = self.scope
 
     def put(self, name, line, symbol_type, data_type=None):
+        print(self.stack)
         """Add a symbol to the current scope."""
-        scope = len(self.stack)-1
-        symbol = Symbol(name, line, symbol_type, scope, data_type)
-        self.stack[-1].put(name, symbol)
-        self.full_stack[-1].put(name, symbol)
+        symbol = Symbol(name, line, symbol_type, self.scope, data_type)
+        self.stack[self.scope].put(name, symbol)
 
     def get(self, name):
         """Get a symbol from the current scope or its parent scopes."""
@@ -59,20 +33,69 @@ class SymbolTable:
 
     def push_scope(self):
         """Push a new scope onto the stack."""
-        self.stack.append(Scope(self.stack[-1]))
-        self.full_stack.append(Scope(self.full_stack[-1]))
+        self.scope += 1
+        self.scopeTOTAL += 1
+
+        if self.scope < self.scopeTOTAL:
+
+            self.scope = self.scopeTOTAL
+            
+            self.stack.append(Scope(self.stack[-1]))
+
+        else:
+            self.scope -= 1
+            
 
 
     def pop_scope(self):
         """Remove and return the current scope from the stack."""
-        return self.stack.pop()
+        self.scope -= 1
     
     def print_table(self):
         """Print the content of the symbol table."""
         print()
-        for index, scope in enumerate(self.full_stack):
-            print("Scope Level:", index)
+        for index, scope in enumerate(self.stack):
             for name, symbol in scope.symbols.items():
                 print(f"  Name: {symbol.name}, Type: {symbol.symbol_type}, Line: {symbol.line}, Scope: {symbol.scope}, Data Type: {symbol.data_type}")
             print()
 
+
+
+
+class Scope:
+    def _init_(self, parent=None):
+        self.parent = parent
+        self.entries = {}
+
+    def insert(self, identifier, info):
+        self.entries[identifier] = info
+
+    def lookup(self, identifier):
+        return self.entries.get(identifier, None)
+
+class SymbolTable:
+    def _init_(self):
+        self.current_scope = Scope()
+
+    def enter_scope(self):
+        new_scope = Scope(self.current_scope)
+        self.current_scope = new_scope
+
+    def exit_scope(self):
+        if self.current_scope.parent:
+            self.current_scope = self.current_scope.parent
+        else:
+            print("Warning: Already at the outermost scope!")
+
+
+    def insert(self, identifier, info):
+        self.current_scope.insert(identifier, info)
+
+    def lookup(self, identifier):
+        scope = self.current_scope
+        while scope:
+            info = scope.lookup(identifier)
+            if info is not None:
+                return info
+            scope = scope.parent
+        return None
